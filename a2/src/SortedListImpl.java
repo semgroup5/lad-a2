ecf3c4889684bb0ecacbb48121e2fd6929f2a179
Jonatan <jonatan.petur@gmail.com>
2016-02-10 12:48:29 +0100

    Started Task 2

---
+++ a2/src/SortedListImpl.java
@@ -0,0 +1,50 @@
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+/**
+ * Created by jpp on 10/02/16.
+ */
+public class SortedListImpl implements SortedList {
+
+    LinkedList<Comparable> list;
+
+    @Override
+    public void add(Comparable elem) {
+
+    }
+
+    @Override
+    public void addSortedArray(Comparable[] arr) {
+
+    }
+
+    @Override
+    public Comparable get(int ix) {
+        return null;
+    }
+
+    @Override
+    public int firstIndex(Comparable elem) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndex(Comparable elem) {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Comparable elem) {
+        return false;
+    }
+
+    @Override
+    public int countBetween(Comparable lo, Comparable hi) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
